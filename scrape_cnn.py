b7aa7485482ba58fc0e74d2f6b966284dea8dcb5
Lucas Falcao <lughfalcao@gmail.com>
2023-11-08 14:45:58 -0300

    main

---
+++ scrape_cnn.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_cnn(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    headline = soup.find(class_="headline__text inline-placeholder")
+    content = soup.find(class_="article__content").findChildren(class_="paragraph inline-placeholder")
+    
+    print(headline.getText().strip())
+    for child in content:
+        print(child.text.strip())
+        
+if __name__ == "__main__":
+    url = input("Enter the URL of the CNN article: ")
+    scrape_cnn(url)
